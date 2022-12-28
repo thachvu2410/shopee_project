75f15f316fc072f5e2d78f0fb51e8f8b13f2972a
thachvu2410 <quangthach31@gmail.com>
2022-12-28 09:32:49 +0700

    upload first time source code

---
+++ src/main/java/com/cybersoft/shopee_project/ShopeeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cybersoft.shopee_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/shopee_project/ShopeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.shopee_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
